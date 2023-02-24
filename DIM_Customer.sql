c9b67c442ff7a778646dd542fca9f13d3c5d3c83
haotran0813 <125161886+haotran0813@users.noreply.github.com>
2023-02-23 17:45:20 -0800

    Add files via upload

---
+++ DIM_Customer.sql
@@ -0,0 +1,42 @@
+-- Cleansed Dim_Customer Table --
+SELECT 
+[CustomerKey], 
+--[GeographyKey], 
+--[CustomerAlternateKey], 
+--[Title], 
+c.firstname as [FirstName], 
+--[MiddleName], 
+c.lastname as [LastName], 
+c.FirstName + ' ' + c.LastName as [Fullname],
+--[NameStyle], 
+--[BirthDate], 
+--[MaritalStatus], 
+--[Suffix], 
+CASE c.gender 
+	WHEN 'M' THEN 'Male'
+	WHEN 'F' THEN 'FEMALE'
+	END AS [Gender], 
+--[EmailAddress], 
+--[YearlyIncome], 
+--[TotalChildren], 
+--[NumberChildrenAtHome], 
+--[EnglishEducation], 
+--[SpanishEducation], 
+--[FrenchEducation], 
+--[EnglishOccupation], 
+--[SpanishOccupation], 
+--[FrenchOccupation], 
+--[HouseOwnerFlag], 
+--[NumberCarsOwned], 
+--[AddressLine1], 
+--[AddressLine2], 
+--[Phone], 
+c.DateFirstPurchase as DateFirstPurchase,
+--[CommuteDistance] 
+g.city as CustomerCity
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimCustomer] as c
+  LEFT JOIN dbo.DimGeography as g
+  ON c.GeographyKey = g.GeographyKey
+  Order by CustomerKey ASC
+
